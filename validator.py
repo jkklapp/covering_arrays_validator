aa1f117f99824387cddf5ac6470a9678bd36ea14
Jaakko Lappalainen <jaakkol@jaakkol-macbookair.roam.corp.google.com>
2015-07-04 13:19:14 +0100

    Added multiprocessing for column_covers function in validator module.

--- validator.py
+++ validator.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from multiprocessing import Pool
 
 def get_column(matrix, c):
     '''
@@ -6,11 +7,14 @@ def get_column(matrix, c):
     '''
     return [x[c] for x in matrix]
 
-def columns_cover(columns, v, t):
+def columns_cover(args=[]):
     '''
         For the selected columns, check
         if they have all possible v-tuples.
     '''
+    columns = args[0]
+    v = args[1]
+    t = args[2]
     found_combinations = []
     for i in range(t):
         input = [c[i] for c in columns]
@@ -23,9 +27,12 @@ def covers(matrix, v, k, t):
         Given a matrix computes the covering
         check for all v-sets of columns.
     '''
-    validity = []
+    p = Pool(k)
+    args = []
     for c in combinations([i for i in range(k)], v):
         c = list(c)
-        columns = [get_column(matrix, col) for col in c]
-        validity.append(columns_cover(columns, v, t))
+        args.append([[get_column(matrix, col) for col in c], v, t])
+    validity = [p.map(columns_cover, args)]
+    p.close()
+    p.join()
     return False not in validity
